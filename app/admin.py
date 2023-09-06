d5803f1f5e60d4c1a5b9884633119c4d928fd1c6
Keeyoshi Pyakurel <keeyoshipyakurel@Keeyoshi.local>
2023-09-06 10:41:18 +0545

    first commit

---
+++ app/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Customer, Product, Cart, OrderPlaced
+
+# Register your models here.
+
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "locality", "address", "zipcode", "city"]
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "selling_price",
+        "discounted_price",
+        "description",
+        "brand",
+        "category",
+        "product_image",
+    ]
+
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "product", "quantity"]
+
+
+@admin.register(OrderPlaced)
+class OrderPlacedAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "customer",
+        "product",
+        "quantity",
+        "order_date",
+        "status",
+    ]
---
+++ app/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from .models import Customer, Product, OrderPlaced, Cart
+from django.views import View
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
+
+
+# Create your views here.
+class ProductView(View):
+    def get(self, request):
+        topwears = Product.objects.filter(category="TW")
+        mobiles = Product.objects.filter(category="M")
+        laptops = Product.objects.filter(category="L")
+        bottomwears = Product.objects.filter(category="BW")
+        accessorys = Product.objects.filter(category="A")
+
+        context = {
+            "topwears": topwears,
+            "mobiles": mobiles,
+            "laptops": laptops,
+            "bottomwears": bottomwears,
+            "accessorys": accessorys,
+        }
+        return render(request, "app/home.html", context)
+
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        context = {"product": product}
+        return render(request, "app/productdetail.html", context)
+
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, "app/customerregistration.html", {"form": form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Congratulations!! Registered Successfully")
+            form.save()
+        return render(request, "app/customerregistration.html", {"form": form})
+
+
+def add_to_cart(request):
+    return render(request, "app/addtocart.html")
+
+
+def buy_now(request):
+    return render(request, "app/buynow.html")
+
+
+def profile(request):
+    return render(request, "app/profile.html")
+
+
+def address(request):
+    return render(request, "app/address.html")
+
+
+def orders(request):
+    return render(request, "app/orders.html")
+
+
+def change_password(request):
+    return render(request, "app/changepassword.html")
+
+
+def mobile(request, data=None):
+    if data is None:
+        mobiles = Product.objects.filter(category="M")
+    elif data in ["Iphone", "Samsung"]:
+        mobiles = Product.objects.filter(category="M").filter(brand=data)
+    elif data == "below":
+        mobiles = Product.objects.filter(category="M").filter(
+            discounted_price__lt=10000
+        )
+    elif data == "above":
+        mobiles = Product.objects.filter(category="M").filter(
+            discounted_price__gt=10000
+        )
+    context = {"mobiles": mobiles}
+    return render(request, "app/mobile.html", context)
+
+
+def checkout(request):
+    return render(request, "app/checkout.html")
